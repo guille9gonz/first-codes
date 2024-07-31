c53827ec3a7506542ae6d07721752b99f11ce6ce
unknown <guillermogcordero@gmail.com>
2024-07-31 10:55:00 +0200

    Added some comments

--- EmailValidator.py
+++ EmailValidator.py
@@ -1,16 +1,22 @@
 def validate(email):
     wrong = "Email is invalid"
+
+    #Check if the email contains just one "@" and at least a dot
     if email.count("@") != 1 or email.count(".") == 0:
         return wrong
-    recipient = email.split("@")[0]
+    
+    #Split the email in three parts (username, domain and top-level domain)
+    username = email.split("@")[0]
     domain = email.split("@")[1].split(".")[0]
     toplevel = email.split("@")[1].split(".")[1]
     special = [".", "_"]
     hyphen = "-"
     toplevel_names = ["com", "net", "org", "tech", "es"]
-    if len(recipient) < 3 or len(recipient) > 24 or recipient[0] in special or recipient[-1] in special:
+
+    #Check if the username and the domain have an appropriate length and only contain allowed characters
+    if len(username) < 3 or len(username) > 24 or username[0] in special or username[-1] in special:
         return wrong
-    for l in recipient:
+    for l in username:
         if not l.isalnum() and l not in special:
             return wrong
     if len(domain) < 3 or len(domain) > 12:
@@ -20,6 +26,8 @@ def validate(email):
             return wrong
     if toplevel not in toplevel_names:
         return wrong
+    
+    #If the address passes all the filters, it returns "Email is valid"
     return "Email is valid"
 
 def main():
