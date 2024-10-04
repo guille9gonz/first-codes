7419a1662b9729915bd6b703a4b1c99a98fd8fc9
Guillermo <guillermogcordero@gmail.com>
2024-10-04 10:03:03 +0200

    Add integers package

---
+++ go/learn/integers/adder_test.go
@@ -0,0 +1,23 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected %d but got %d", expected, sum)
+	}
+}
+
+// Testable functions begin with Example
+// Output, despite commented, is important
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
